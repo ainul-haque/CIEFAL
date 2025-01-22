5798b46add478f0360be36aff4af93298080a77f
Alan-Abdul-Kalaam-25 <alanabdulkalaam@gmail.com>
2025-01-22 14:41:12 +0530

    added CDN links to analytics and a favicon.png

---
+++ public/scripts/vercel-analytics.js
@@ -0,0 +1,7 @@
+import { inject } from "https://cdn.vercel-insights.com/v1/script.js";
+import { injectSpeedInsights } from "https://cdn.vercel-insights.com/v1/speed-insights.js";
+
+inject();
+injectSpeedInsights();
+
+console.log("Vercel Analytics and Speed Insights initialized");
