7f357e367632ceb53b8e762d7244c5bd38eadd1b
Alan-Abdul-Kalaam-25 <alanabdulkalaam@gmail.com>
2025-02-01 13:58:04 +0530

    testing the hosting working or not

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ import { fileURLToPath } from "node:url";
 import ejsMate from "ejs-mate";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Set up EJS with ejs-mate
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Serve static files from the 'public' directory
-// Update static files middleware
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
@@ -28,7 +24,14 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-// 404 route
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact" });
+});
+
+app.get("/courses", (req, res) => {
+  res.render("courses", { title: "Courses" });
+});
+
 app.use((req, res) => {
   res.status(404).render("404", { title: "404 - Not Found" });
 });
